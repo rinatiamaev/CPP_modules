b581bb6515df197672bb87bc2b3005c8d1ff4f4a
admin <izhairguns@ubuntu.myguest.virtualbox.org>
2025-01-05 10:44:49 +0200

    ex00 changing

--- module 00/ex00/megaphone.cpp
+++ module 00/ex00/megaphone.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
+#include <string>
+
+void GetStrings(int ac, char **av)
+{
+	for (int i = 1; i < ac; i++)
+	{
+		std::string str = av[i];
+		for (int j = 0; j < (int)str.length(); j++)
+		{
+			str[j] = std::toupper(str[j]);
+		}
+		std::cout << str;
+	}
+}
 
 int main(int ac, char **av)
 {
     if (ac == 1)
     {
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
     }
     else
     {
-        for (int i = 1; i < ac; i++)
-        {
-            for (int j = 0; av[i][j]; j++)
-            {
-                std::cout << (char)toupper(av[i][j]);
-            }
-        }
-        std::cout << std::endl;
+    	GetStrings(ac, av);
+    	std::cout << std::endl;
     }
 }
