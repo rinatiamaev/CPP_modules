6a6ac7465d35df5c5bf746be424a2863fc84f429
rinat iamaev <rinatiamaev@gmail.com>
2025-02-25 22:34:08 +0200

    module 02 ex00 canonic style adding

---
+++ module 02/ex00/Fixed.cpp
@@ -0,0 +1,30 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed():fix_point(0){
+    std::cout<<"Default constructor called\n";
+}
+
+Fixed::Fixed(const Fixed& other){
+    std::cout<<"Copy constructor called\n";
+    this->fix_point = other.fix_point;
+}
+
+Fixed& Fixed::operator=(const Fixed& other){
+    std::cout<<"Copy assignment operator called\n";
+    if (this != &other)
+        this->fix_point = other.fix_point;
+    return (*this);
+}
+
+int Fixed::getRawBits() const{
+    std::cout<<"getRawBits member function called\n";
+    return (this->fix_point);
+}
+
+void Fixed::setRawBits(int const raw){
+    this->fix_point = raw;
+}
+
+Fixed::~Fixed(){
+    std::cout<<"Destructor called\n";
+}
--- module 02/ex00/Fixed.hpp
+++ module 02/ex00/Fixed.hpp
@@ -1,16 +1,20 @@
 #ifndef FIXED_HPP
 #define FIXED_HPP
 
+#include <iostream>
+
 class Fixed{
     private:
         static const int FRACT_BIT = 8;
         int fix_point;
 
     public:
-        Fixed(): fix_point(0){}
-        ~Fixed(){}
+        Fixed();
+        ~Fixed();
+        Fixed(const Fixed& other);
         int getRawBits() const;
         void setRawBits(int const raw);
+        Fixed& operator=(const Fixed& other);
 };
 
 #endif
