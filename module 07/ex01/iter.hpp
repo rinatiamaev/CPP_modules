5ba896d1ce4fd13292de6848026392d0706d1107
Rinat Iamaev <riamaev@c1r6p14.hive.fi>
2025-11-21 11:10:25 +0200

    adding main from eval

--- module 07/ex01/iter.hpp
+++ module 07/ex01/iter.hpp
@@ -4,13 +4,10 @@
 #include <cstddef>
 
 template <typename T>
-void iter(T *array, size_t length, void (*func)(T &)) {
-	if (!array || !func)
-		return;
 
-	for (size_t i = 0; i < length; i++) {
+void iter(T *array, size_t length, void (*func)(const T &)) {
+	for (size_t i = 0; i < length; ++i)
 		func(array[i]);
-	}
 }
 
 #endif
